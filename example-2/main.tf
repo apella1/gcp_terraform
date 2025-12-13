7c5f9435470f001f5ac3df8c784c79f8c9f11402
John Apella <johncollons5@gmail.com>
2025-12-13 10:53:58 +0300

    feat: associate static ip to vm instance

--- example-2/main.tf
+++ example-2/main.tf
@@ -1,39 +1,41 @@
 terraform {
-    required_providers {
-        google = {
-            source = "hashicorp/google"
-            version = "3.5.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
     }
+  }
 }
 
 provider "google" {
   project = "qwiklabs-gcp-01-813e61639211"
-  region = "europe-west1"
-  zone = "europe-west1-c"
+  region  = "europe-west1"
+  zone    = "europe-west1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
-    name = "terraform_network"
+  name = "terraform_network"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name = "terraform-instance"
+  name         = "terraform-instance"
   machine_type = "e2-micro"
-  tags = ["web", "dev"]
+  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-        image = "debian-cloud/debian-12"
+      image = "debian-cloud/debian-12"
     }
   }
 
   network_interface {
     network = google_compute_network.vpc_network.name
-    access_config {}
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
   }
 }
 
 resource "google_compute_address" "vm_static_ip" {
-    name = "terraform-static-ip"
+  name = "terraform-static-ip"
 }
