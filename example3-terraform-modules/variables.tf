5e95e484eb9e67c6d94e4d32ca6c7c4208607255
John Apella <johncollons5@gmail.com>
2025-12-14 12:33:50 +0300

    feat: add a terraform module

---
+++ example3-terraform-modules/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "qwiklabs-gcp-03-fcb183c298d4"
+}
+
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "qwiklabs-gcp-03-fcb183c298d4"
+}
