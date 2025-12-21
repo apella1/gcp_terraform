d46dee231d472b41d457f01367bbcda59923618a
John Apella <johncollons5@gmail.com>
2025-12-21 14:19:10 +0300

    add vpc module from google terraform registry

--- terraform-challenge-lab/task1/main.tf
+++ terraform-challenge-lab/task1/main.tf
@@ -21,8 +21,6 @@ module "storage" {
       with_state = "ANY"
     }
   }]
-
-
 }
 
 module "instances" {
@@ -45,6 +43,29 @@ To import the instances from the console with
 terraform import 'module.instances["tf-instance-1"].google_compute_instance.vm_instance' tf-instance-1
 terraform import 'module.instances["tf-instance-2"].google_compute_instance.vm_instance' tf-instance-2
 */
+
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "10.0.0"
+
+  project_id   = var.project_id
+  network_name = "tf-vpc-381802"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-east1"
+    },
+    {
+      subnet_name   = "subnet-02"
+      subnet_ip     = "10.10.20.0/24"
+      subnet_region = "us-east1"
+    },
+  ]
+
+}
 
 terraform {
   backend "gcs" {
